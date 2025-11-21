81f2f0c846e39fe66264320b5cb775f87afdd4c5
RyanDinger <rlheidinger@icloud.com>
2025-11-20 17:48:28 -0700

    Initial code commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <header className="App-header">
+          <label>Text Analyzer</label>
+          <textarea className="text-input" placeholder="Enter the text you need analyzed..." />
+          <button>Analyze Text</button>
+        </header>
+      </div>
     </div>
   );
 }
