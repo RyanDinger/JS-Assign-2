b6c1c8f922abf1a7c87e69ea1a2d18084d020dad
Jaren-Wheeler <git@github.com>
2025-11-21 19:55:32 -0700

    Included some styling

--- src/App.js
+++ src/App.js
@@ -6,32 +6,39 @@ function App() {
 
   const [text, setText] = useState("");
   const [wordArray, setWordArray] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+  const [btnHover, setBtnHover] = useState(false);
+
   const analyzeText = () => {
     // split every part of the string in the textarea into an array of individual, case-sensitive words.
     const words = text.toLowerCase().split(/[^a-zA-Z0-9']+/).filter(w => w.length > 0); // regular expression allows for apostrophes
     setWordArray(words);
+    setShowResults(true);
     console.log(words);
   }
+ 
   return (
-    <div className="App">
-      <div className="App">
+      <div className="App" style={{padding: '2rem'}}>
         <header className="App-header">
-          <label>Text Analyzer</label>
+          <label style={{padding: '1rem', fontSize: '32px', fontWeight: 'bold', color: 'greenyellow'}}>Text Analyzer</label>
           <textarea 
             className="text-input" 
             placeholder="Enter the text you need analyzed..."
             value={text}
             onChange={(e) => setText(e.target.value)}/>
-          <button onClick={analyzeText}>Analyze Text</button>
+          <button 
+            onClick={analyzeText} 
+            style={{backgroundColor: 'greenyellow', fontSize: '20px', borderRadius: '5px', transform: btnHover ? 'scale(1.05)' : 'scale(1)'}}
+            onMouseEnter={() => setBtnHover(true)}
+            onMouseLeave={() => setBtnHover(false)}
+            >Analyze Text
+          </button>
         </header>
          <p style={{color: 'white'}}>This text contains NUMBER OF SENTENCES</p>
         <div style={{display: 'flex', justifyContent: 'center'}}>
-          <WordFrequency wordArray={wordArray}></WordFrequency>
+          {showResults && <WordFrequency wordArray={wordArray} />} {/* Conditionally render the word frequency if the showResults flag is also true */}
         </div>
-       
       </div>
-    </div>
-    
   );
 }
 
